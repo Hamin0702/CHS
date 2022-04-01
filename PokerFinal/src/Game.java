acbf804445fb9e752bb3e22902523ef98ce2753e
Hamin Han <33069589+Hamin0702@users.noreply.github.com>
2022-04-01 01:55:30 -0400

    Add files via upload

---
+++ PokerFinal/src/Game.java
@@ -0,0 +1,242 @@
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.ActionListener;
+
+public class Game extends FlexiblePictureExplorer implements ImageObserver{
+	
+	private boolean check = false;
+	private final String path = "cards/";
+	private String playerRank="";
+	
+	ArrayList<Card> tableCards = new ArrayList<Card>();
+	ArrayList<Card> playerCards = new ArrayList<Card>();
+	ArrayList<Card> compCards = new ArrayList<Card>();
+	ArrayList<Card> combined = new ArrayList<Card>();
+	
+	Picture disp = new Picture(800,1358);
+	Graphics2D graphics = disp.createGraphics();
+	
+	private String[][] images = { {"table","blue_back", "play","bet","stop"},
+								  {"AH", "AS", "AC", "AD"},
+								  {"2H", "2S", "2C", "2D"},
+								  {"3H", "3S", "3C", "3D"},
+								  {"4H", "4S", "4C", "4D"},
+								  {"5H", "5S", "5C", "5D"},
+								  {"6H", "6S", "6C", "6D"},
+								  {"7H", "7S", "7C", "7D"},
+								  {"8H", "8S", "8C", "8D"},
+								  {"9H", "9S", "9C", "9D"},
+								  {"10H", "10S", "10C", "10D"},
+								  {"JH", "JS", "JC", "JD"},
+								  {"QH", "QS", "QC", "QD"},
+								  {"KH", "KS", "KC", "KD"} };
+	
+	Game(HandEvaluator myHand) {
+		super(new Picture(800,1358));
+		playerRank = myHand.getRank();
+		combined = myHand.getCombined();
+		compCards = myHand.getComputer();
+		tableCards = myHand.getTable();
+		playerCards = myHand.getPlayerCards();
+		displayGame();
+		displayTable();
+		displayCards();
+		
+	}
+	
+	public void displayGame() {
+		Picture background = new Picture(path + images[0][0] + ".jpg");
+		Picture play = new Picture("cards/play.png");
+		Picture bet = new Picture("cards/bet.png");
+		Picture stop = new Picture("cards/stop.png");
+		graphics.drawImage(background.getBufferedImage(), 0, 0, this);
+		graphics.drawImage(play.getBufferedImage(), 850,60, this);
+		graphics.drawImage(bet.getBufferedImage(), 1000,60, this);
+		graphics.drawImage(stop.getBufferedImage(), 1150,60, this);
+		setImage(disp);
+	}
+	
+	public void displayTable() {
+		if(check) {
+			Picture card1 = new Picture(path + tableCards.get(0).imageName() + ".jpg");
+			Picture card2 = new Picture(path + tableCards.get(1).imageName() + ".jpg");
+			Picture card3 = new Picture(path + tableCards.get(2).imageName() + ".jpg");
+			Picture card4 = new Picture(path + tableCards.get(3).imageName() + ".jpg");
+			Picture card5 = new Picture(path + tableCards.get(4).imageName() + ".jpg");
+			
+			graphics.drawImage(card1.getBufferedImage(), 231, 280, this);
+			graphics.drawImage(card2.getBufferedImage(), 415, 280, this);
+			graphics.drawImage(card3.getBufferedImage(), 598, 280, this);
+			graphics.drawImage(card4.getBufferedImage(), 783, 280, this);
+			graphics.drawImage(card5.getBufferedImage(), 967, 280, this);
+			setImage(disp);
+		} else if(!check) {
+			Picture card1 = new Picture(path + tableCards.get(0).imageName() + ".jpg");
+			Picture card2 = new Picture(path + tableCards.get(1).imageName() + ".jpg");
+			Picture card3 = new Picture(path + tableCards.get(2).imageName() + ".jpg");
+			Picture card4 = new Picture(path + "blue_back" + ".jpg");
+			Picture card5 = new Picture(path + "blue_back" + ".jpg");
+			
+			graphics.drawImage(card1.getBufferedImage(), 231, 280, this);
+			graphics.drawImage(card2.getBufferedImage(), 415, 280, this);
+			graphics.drawImage(card3.getBufferedImage(), 598, 280, this);
+			graphics.drawImage(card4.getBufferedImage(), 783, 280, this);
+			graphics.drawImage(card5.getBufferedImage(), 967, 280, this);
+			setImage(disp);
+		}
+	}
+	
+	public void displayCards(){
+		Picture myCard1 = new Picture(path + playerCards.get(0).imageName() + ".jpg");
+		Picture myCard2 = new Picture(path + playerCards.get(1).imageName() + ".jpg");
+		graphics.drawImage(myCard1.getBufferedImage(), 1015, 550, this);
+		graphics.drawImage(myCard2.getBufferedImage(), 1180, 550, this);
+		if(!check) {
+			Picture ComputerCards = new Picture(path + "blue_back" + ".jpg");
+			graphics.drawImage(ComputerCards.getBufferedImage(), 15, 0, this);
+			graphics.drawImage(ComputerCards.getBufferedImage(), 180, 0, this);
+		}else if(check) {
+			Picture ccard1 = new Picture(path + compCards.get(0).imageName() + ".jpg");
+			Picture ccard2 = new Picture(path + compCards.get(1).imageName() + ".jpg");
+			graphics.drawImage(ccard1.getBufferedImage(), 15, 0, this);
+			graphics.drawImage(ccard2.getBufferedImage(), 180, 0, this);
+		}
+			
+		setImage(disp);
+	}
+	
+	public void setCheck(boolean change) {
+		check = change;
+	}
+	
+	public void askPlayer() {
+		System.out.println("Continue? yes or no");
+		Scanner a = new Scanner (System.in);
+		String input = a.next();
+		if(!input.equalsIgnoreCase("yes") && !input.equalsIgnoreCase("no")) {
+			do{
+				System.out.println("Oops, try again");
+				a = new Scanner (System.in);
+				input = a.next();	
+			} while(!input.equalsIgnoreCase("yes") && !input.equalsIgnoreCase("no"));
+		} 
+		System.out.println();
+		check = input.equalsIgnoreCase("yes");
+		if(input.equalsIgnoreCase("no")) {
+			System.out.println("\n");
+			System.out.print("Computer wins!");
+		}
+		if(check) {
+			System.out.println("\n");
+			displayTable();
+			displayCards();
+		}
+	}
+	
+	public void printBoard(){
+		if(check) {
+			System.out.println("The board contains the following cards:");
+			for(int i =0; i<tableCards.size();i++){
+				System.out.println(i+1 + ": " + tableCards.get(i).toString());
+			}
+			System.out.println("\n");
+		}else if(!check) {
+			System.out.println("The board contains the following cards:");
+			for(int i =0; i<tableCards.size()-2;i++){
+				System.out.println(i+1 + ": " + tableCards.get(i).toString());
+			}
+			System.out.println(4 + ": ???");
+			System.out.println(5 + ": ???");
+			System.out.println("\n");
+		}
+	}
+	
+	public void printCombined() {
+		if(check) {
+			int i = 0;
+			System.out.println("Combined Hand:");
+			for(Card deck: combined) {
+				System.out.println(i+1 + ": " +deck);
+				i++;
+			}
+			System.out.println("\n");
+		}
+	}
+	
+	public void printRank() {
+		if(check) {
+			System.out.print("You got " + playerRank);
+			System.out.println("\n");
+			System.out.println("---------------------------------------------------");
+		}
+	}
+	
+	public void printCompRank() {
+		if(check) {
+			HandEvaluator forComp = new HandEvaluator(compCards, tableCards);
+			System.out.print("Computer got " + forComp.getRank());
+			System.out.println("\n");
+		}
+	}
+	
+	public void printCompCards() {
+		if(check) {
+			System.out.println("\n");
+			System.out.println("Computer has the following cards:");
+			for(int i = 0; i<compCards.size(); i++) {
+				System.out.println(i+1 + ": " +compCards.get(i));
+			}
+			System.out.println("\n");
+		}
+	}
+	
+	public void winner() {
+		boolean playerWin,compWin = false;
+		if(check) {
+			System.out.println("---------------------------------------------------");
+			HandEvaluator forComp = new HandEvaluator(compCards, tableCards);
+			int compRank = forComp.getRankNum();
+			HandEvaluator forplayer = new HandEvaluator(playerCards, tableCards);
+			int playerRank = forplayer.getRankNum();
+			
+			if(playerRank>compRank) {
+				playerWin = true;
+				System.out.println("\n");
+				System.out.print("Player wins!");
+				System.out.println("\n");
+			}else if(compRank>playerRank){
+				compWin = true;
+				System.out.println("\n");
+				System.out.print("Computer wins!");
+				System.out.println("\n");
+			} else if(playerRank==compRank) {
+				System.out.println("\n");
+				System.out.print("It's a tie!");
+				System.out.println("\n");
+			}
+		}
+	}
+	
+	@Override
+	public boolean imageUpdate(Image arg0, int arg1, int arg2, int arg3, int arg4, int arg5) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	public void mouseClickedAction(DigitalPicture pict, Pixel pix) {
+		int x = pix.getX();
+		int y = pix.getY();
+		
+	}
+
+	
+}
---
+++ PokerFinal/src/Table.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+
+public class Table {
+	private ArrayList<Card> board;
+	private boolean check = false;
+	//private Card[] burnCards = new Card[3];
+	
+	Table(Deck play){
+		board = new ArrayList<Card>();
+		for(int i = 0; i<5; i++) {
+			board.add(play.drawFromDeck());
+		}
+	}
+	
+	Table(Card card1, Card card2, Card card3, Card card4, Card card5){
+		board = new ArrayList<Card>();
+		board.add(card1); board.add(card2); board.add(card3); board.add(card4); board.add(card5);
+	}
+	
+	public ArrayList<Card> returnTable() {
+		return board;
+	}
+	
+
+	public void setBoardCard(Card card, int cardNum){
+		board.set(cardNum, card);
+	}
+	
+	public Card getBoardCard(int cardNum){
+		return board.get(cardNum);
+	}
+	
+//	public void setBurnCard(Card card, int cardNum){
+//		this.burnCards[cardNum] = card;
+//	}
+	
+//	public Card getBurnCard(int cardNum){
+//		return this.burnCards[cardNum];
+//	}
+	
+	public int boardSize(){
+		return board.size();
+	}
+	
+	public void Continue(boolean tru) {
+		check=tru;
+	}
+	
+	public boolean getCheck() {
+		return check;
+	}
+	
+	
+	public void printBoard(){
+		System.out.println("The board contains the following cards:");
+		for(int i =0; i<board.size();i++){
+			System.out.println(i+1 + ": " + getBoardCard(i).toString());
+		}
+		System.out.println("\n");
+	}
+	/*
+	public void printBoardUnknown() {
+		System.out.println("The board contains the following cards:");
+		for(int i =0; i<board.size()-2;i++){
+			System.out.println(i+1 + ": " + getBoardCard(i).toString());
+		}
+		System.out.println(4 + ": ???");
+		System.out.println(5 + ": ???");
+		System.out.println("\n");
+	}
+	*/
+//	protected void printBurnCards(){
+//		System.out.println("The burn cards are:");
+//		for(int i =0; i<burnCards.length;i++){
+//			System.out.println(i+1 + ": " + getBurnCard(i).printCard());
+//		}
+//		System.out.println("\n");
+//	}
+
+}
+
+
